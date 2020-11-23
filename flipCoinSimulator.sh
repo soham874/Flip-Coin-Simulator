fa27d0997d349a9640db4ef7d7bdef2b4289396e
soham874 <soham874@gmail.com>
2020-11-23 12:54:11 +0530

    Refactored : Added win conditions

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-read -p "Enter number of times : " number
+#read -p "Enter number of times : " number
 
-for ((i=1;i<=$number;i++))
+headwon=0
+tailwon=0
+
+while [[ $headwon -lt 21 && $tailwon -lt 21 ]]
 do
 	flip=$(($RANDOM%2))
 	if [ $flip -eq 0 ]
 	then 
-		echo "We got a tail"
+		((tailwon++))
 	else
-		echo "We got a head"
+		((headwon++))
 	fi
 done
+
+if [ $headwon -eq 21 ]
+then
+	echo "Head won 21 games"
+	echo "Tail won $tailwon games"
+else
+	echo "Tail won 21 games"
+	echo "Head won $headwon games"
+fi
